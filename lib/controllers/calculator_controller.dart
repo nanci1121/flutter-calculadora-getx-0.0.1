cabdbf87bac508b5cc1d7f963eaccef422049278
nanci1121 <vepimal@yahoo.es>
2021-08-04 18:41:23 +0200

    Calculadora con getx

---
+++ lib/controllers/calculator_controller.dart
@@ -0,0 +1,82 @@
+import 'package:get/get.dart';
+
+class CalculatorController extends GetxController {
+  var firsNumber = '10'.obs;
+  var secondNumber = '20'.obs;
+  var mathResult = '30'.obs;
+  var operation = '+'.obs;
+
+  resetAll() {
+    firsNumber.value = '0';
+    secondNumber.value = '0';
+    mathResult.value = '0';
+    operation.value = '+';
+  }
+
+  changeNegativePositive() {
+    if (mathResult.startsWith('-')) {
+      mathResult.value = mathResult.value.replaceAll('-', '');
+    } else {
+      mathResult.value = '-' + mathResult.value;
+    }
+  }
+
+  addNumber(String number) {
+    if (mathResult.value == '0') return mathResult.value = number;
+    if (mathResult.value == '-0') {
+      return mathResult.value = '-' + number;
+    }
+    mathResult.value = mathResult.value + number;
+  }
+
+  addDecimalPoint() {
+    if (mathResult.contains('.')) {
+      return;
+    }
+    if (mathResult.startsWith('0')) {
+      mathResult.value = '0.';
+    } else {
+      mathResult.value = mathResult.value + '.';
+    }
+  }
+
+  selectOperation(String newOperation) {
+    operation.value = newOperation;
+    firsNumber.value = mathResult.value;
+    mathResult.value = '0';
+  }
+
+  deleteLastEntry() {
+    if (mathResult.replaceAll('-', '').length > 1) {
+      mathResult.value =
+          mathResult.value.substring(0, mathResult.value.length - 1);
+    } else {
+      mathResult.value = '0';
+    }
+  }
+
+  calculateResult() {
+    double num1 = double.parse(firsNumber.value);
+    double num2 = double.parse(mathResult.value);
+    secondNumber.value = mathResult.value;
+
+    switch (operation.value) {
+      case '+':
+        mathResult.value = '${num1 + num2}';
+        break;
+      case '-':
+        mathResult.value = '${num1 - num2}';
+        break;
+      case 'X':
+        mathResult.value = '${num1 * num2}';
+        break;
+      case '/':
+        mathResult.value = '${num1 / num2}';
+        break;
+      default:
+        break;
+    }
+    if (mathResult.value.endsWith('.0')){
+      mathResult.value= mathResult.value.substring(0,mathResult.value.length -2);
+  }
+}
